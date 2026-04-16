d7db716ee703a9ca54c436c895f31a725bff6a0d
Prabhjot <prabhjotk0187@gmail.com>
2026-04-15 23:23:57 -0500

    initil the js code

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const box1 = document.getElementById('box1');
+const box2 = document.getElementById('box2');
+const overlay = document.getElementById('overlay');
+
+const acceptBtn = document.getElementById('acceptBtn');
+const settingsBtn = document.getElementById('settingsBtn');
+const saveBtn = document.getElementById('saveBtn');
+const rejectBtn = document.getElementById('rejectBtn');
+
+const chkBrowser = document.getElementById('chkBrowser');
+const chkOS = document.getElementById('chkOS');
+const chkScreen = document.getElementById('chkScreen');
